3cc972bd1d9430441d5402af62b79f1b88eeb0db
Miguel Bayón <publi.michelbayon@gmail.com>
2017-07-13 12:40:46 +0200

    Añadido un ejemplo de uso de la funcion obtenerJSONDesdeSpreadsheetAgrupadoPor

--- examples.js
+++ examples.js
@@ -1,4 +1,38 @@
-function ejemplo1() {
+/**
+Para probar estas ejecuciones debes:
+- Cambiar el nombre de la funcion a doGet.
+- Poner IDs de hoja de Google Sheet sobre los que tengas permiso
+**/
+
+function ejemploInvocacion_obtenerJSONDesdeSpreadsheetAgrupadoPor()
+{
+  var columnasDeseadasYNoDeseadas = {columnasDeseadas: ['*'], columnasNoDeseadas: []};
+  var objetoADevolver = obtenerJSONDesdeSpreadsheetAgrupadoPor('1d0zQXl7zgW07fpNcu5fJ6tq3NsearNnrTEVZJMcFDR4',
+                                                    '0',
+                                                    columnasDeseadasYNoDeseadas, 
+                                                    function(elemento) { return (elemento == 'AD04');},
+                                                    "horarios-20016",
+                                                    false,
+                                                    true,
+                                                    'profesor');
+  Logger.log(JSON.stringify(objetoADevolver));
+                                            
+  var medianteJSONP = false;
+  var prefijo = ''; 
+  
+  if (medianteJSONP) {
+    return ContentService.createTextOutput(prefijo + '(' + JSON.stringify(objetoADevolver) + ')').setMimeType(ContentService.MimeType.JAVASCRIPT);
+  }
+  else {
+    return ContentService.createTextOutput(JSON.stringify(objetoADevolver)).setMimeType(ContentService.MimeType.JSON);     
+  }   
+}
+
+
+
+
+
+function ejemploInvocacion_addFilaDesdeJSON() {
   addFilaDesdeJSON('17G_AALniqiYQoehpXpHYHI5UVuvdIkaSaNaTFuN7_gQ',
     '0', {
       anno: 2016,
@@ -12,7 +46,7 @@ function ejemplo1() {
 
 
 
-function ejemplo2() {
+function ejemploInvocacion_obtenerJSONDesdeSpreadsheet() {
   var columnasDeseadasYNoDeseadas = {
     columnasDeseadas: ['codigo', 'materia', 'abreviada'],
     columnasNoDeseadas: []
@@ -34,7 +68,7 @@ function ejemplo2() {
 
 
 
-function ejemplo3() {
+function ejemploInvocacion_obtenerJSONDesdeSpreadsheet() {
   var columnasDeseadasYNoDeseadas = {
     columnasDeseadas: ['*'],
     columnasNoDeseadas: []
